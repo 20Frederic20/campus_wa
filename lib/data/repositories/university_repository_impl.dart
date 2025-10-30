e132b0d1e3363a5017c14f3572c2df96259deb8d
20Frederic20 <fredericpierrothountinme@gmail.com>
2025-10-30 01:35:17 +0100

    use astract clas and implements

--- lib/data/repositories/university_repository_impl.dart
+++ lib/data/repositories/university_repository_impl.dart
@@ -1,15 +1,17 @@
 import 'package:dio/dio.dart';
-import '../models/api/university_dto.dart';
-import '../services/api_service.dart';
-import '../../domain/models/university.dart';
+import 'package:campus_wa/data/models/api/university_dto.dart';
+import 'package:campus_wa/data/services/api_service.dart';
+import 'package:campus_wa/domain/models/university.dart';
+import 'package:campus_wa/domain/repositories/university_repository.dart';
 
-class UniversityRepository {
+class UniversityRepositoryImpl implements UniversityRepository {
   final ApiService _apiService = ApiService();
 
+  @override
   Future<List<University>> getUniversities() async {
     try {
       final response = await _apiService.get('/universities');
-      
+
       // Si l'API renvoie une liste d'universités
       if (response.data is List) {
         return (response.data as List)
@@ -22,7 +24,7 @@ class UniversityRepository {
             .map((json) => UniversityDto.fromJson(json).toDomain())
             .toList();
       }
-      
+
       throw Exception('Format de réponse inattendu');
     } on DioException catch (e) {
       // Gestion des erreurs spécifiques
@@ -33,6 +35,7 @@ class UniversityRepository {
     }
   }
 
+  @override
   Future<University> getUniversityById(String id) async {
     try {
       final response = await _apiService.get('/universities/$id');
@@ -41,6 +44,28 @@ class UniversityRepository {
       if (e.response?.statusCode == 404) {
         throw Exception('Université non trouvée');
       }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<University>> searchUniversities(String query) async {
+    try {
+      final response =
+          await _apiService.get('/universities/search', params: {'q': query});
+
+      if (response.data is List) {
+        return (response.data as List)
+            .map((json) => UniversityDto.fromJson(json).toDomain())
+            .toList();
+      } else if (response.data is Map && response.data['data'] != null) {
+        return (response.data['data'] as List)
+            .map((json) => UniversityDto.fromJson(json).toDomain())
+            .toList();
+      }
+
+      throw Exception('Format de réponse inattendu');
+    } on DioException catch (e) {
       rethrow;
     }
   }
