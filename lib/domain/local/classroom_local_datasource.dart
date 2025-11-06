fb4951a49510fa5f602e96e0650519bb0c38e29d
20Frederic20 <fredericpierrothountinme@gmail.com>
2025-11-06 15:06:11 +0100

    add random class cache implements in local store

--- lib/domain/local/classroom_local_datasource.dart
+++ lib/domain/local/classroom_local_datasource.dart
@@ -3,7 +3,21 @@ import 'package:campus_wa/data/models/api/classroom_dto.dart';
 abstract class ClassroomLocalDataSource {
   Future<void> cacheClassrooms(List<ClassroomDto> classrooms);
   Future<List<ClassroomDto>?> getCachedClassrooms();
-  Future<void> cacheClassroomsByUniversityId(String universityId, List<ClassroomDto> classrooms);
-  Future<List<ClassroomDto>?> getCachedClassroomsByUniversityId(String universityId);
+  Future<void> cacheClassroomsByUniversityId(
+    String universityId,
+    List<ClassroomDto> classrooms,
+  );
+  Future<List<ClassroomDto>?> getCachedClassroomsByUniversityId(
+    String universityId,
+  );
+  Future<void> cacheClassroomById(String id, ClassroomDto classroom);
+  Future<ClassroomDto?> getCachedClassroomById(String id);
+  Future<void> cacheClassroomsByIds(
+    List<String> ids,
+    List<ClassroomDto> classrooms,
+  );
+  Future<List<ClassroomDto>?> getCachedClassroomsByIds(List<String> ids);
+  Future<void> cacheRandomClassrooms(List<ClassroomDto> classrooms);
+  Future<List<ClassroomDto>?> getCachedRandomClassrooms();
   Future<void> clearCache();
 }
