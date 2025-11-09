826768c877c3efb6535bf29f7649c87b2fd3abb9
20Frederic20 <fredericpierrothountinme@gmail.com>
2025-11-09 09:06:23 +0100

    add function to calculate distance between two or multiple points

--- lib/core/utils/map_utils.dart
+++ lib/core/utils/map_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -32,4 +34,39 @@ Future<void> openGoogleMaps({
       ),
     );
   }
+}
+
+double _degreesToRadians(double degrees) => degrees * (pi / 180.0);
+
+double haversineDistanceKm(double lat1, double lon1, double lat2, double lon2) {
+  const earthRadiusKm = 6371.0;
+  final dLat = _degreesToRadians(lat2 - lat1);
+  final dLon = _degreesToRadians(lon2 - lon1);
+  final a =
+      (sin(dLat / 2) * sin(dLat / 2)) +
+      (cos(_degreesToRadians(lat1)) *
+          cos(_degreesToRadians(lat2)) *
+          sin(dLon / 2) *
+          sin(dLon / 2));
+  final c = 2 * atan2(sqrt(a), sqrt(1 - a));
+  return earthRadiusKm * c;
+}
+
+List<T> filterWithinKm<T>(
+  List<T> items,
+  double centerLat,
+  double centerLon,
+  double maxKm,
+  double Function(T) latitude,
+  double Function(T) longitude,
+) {
+  return items.where((it) {
+    final d = haversineDistanceKm(
+      centerLat,
+      centerLon,
+      latitude(it),
+      longitude(it),
+    );
+    return d <= maxKm;
+  }).toList();
 }
