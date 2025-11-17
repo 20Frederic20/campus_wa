27d69199d06ef2d33301fbb40a51e28cd212aa2d
20Frederic20 <fredericpierrothountinme@gmail.com>
2025-11-17 01:17:56 +0100

    add news dto

---
+++ lib/data/models/api/news_dto.dart
@@ -0,0 +1,65 @@
+import 'package:campus_wa/domain/models/news.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'news_dto.g.dart';
+
+@JsonSerializable()
+class NewsDto {
+
+  factory NewsDto.fromJson(Map<String, dynamic> json) =>
+      _$NewsDtoFromJson(json);
+
+  factory NewsDto.create({
+    required String id,
+    required String title,
+    String? content,
+    bool? isPublished,
+    DateTime? publishedAt,
+    List<String>? filesUrls,
+  }) {
+    return NewsDto(
+      id: id,
+      title: title,
+      content: content,
+      isPublished: isPublished,
+      publishedAt: publishedAt,
+      filesUrls: filesUrls,
+    );
+  }
+  const NewsDto({
+    required this.id,
+    required this.title,
+    this.content,
+    this.isPublished,
+    this.publishedAt,
+    this.filesUrls,
+  });
+  @JsonKey(name: 'id')
+  final String id;
+
+  @JsonKey(name: 'title')
+  final String title;
+
+  @JsonKey(name: 'content')
+  final String? content;
+
+  @JsonKey(name: 'is_published')
+  final bool? isPublished;
+
+  @JsonKey(name: 'published_at')
+  final DateTime? publishedAt;
+
+  @JsonKey(name: 'files_urls')
+  final List<String>? filesUrls;
+
+  Map<String, dynamic> toJson() => _$NewsDtoToJson(this);
+
+  News toDomain() => News(
+    id: id,
+    title: title,
+    content: content,
+    isPublished: isPublished,
+    publishedAt: publishedAt,
+    filesUrls: filesUrls,
+  );
+}
